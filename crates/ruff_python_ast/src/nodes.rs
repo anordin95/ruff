7a6dca9c7a5747705b53844d9343a0c6096ca7f7
Douglas Creager <dcreager@dcreager.net>
2025-01-31 16:32:03 -0500

    Add TDD implementation

--- crates/ruff_python_ast/src/nodes.rs
+++ crates/ruff_python_ast/src/nodes.rs
@@ -3504,7 +3504,7 @@ impl From<Identifier> for Name {
     }
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug, Hash, PartialEq)]
 pub enum Singleton {
     None,
     True,
